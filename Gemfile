f198200c5dcdf34dde7cfd1140fb43ba4fe338f3
Nadira Muhammad-Smith <nmuhammadsmith@nyc.yearup.org>
2020-01-02 16:51:26 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org' do
+gem 'sinatra-activerecord'
+gem "sqlite3"
+gem "sinatra-flash"
+gem "rake"
+gem 'bcrypt', '~> 3.1', '>= 3.1.13'
+end
---
+++ server.rb
@@ -0,0 +1,58 @@
+require 'sinatra/activerecord'
+require 'sinatra/flash'
+require 'sinatra'
+require './model'
+
+set :port, 3000
+set :database, {adapter: "sqlite3", database: "doggly2.sqlite3"}
+enable :sessions
+
+get '/' do
+  erb :home
+end
+
+get '/login' do
+  erb :login
+end
+
+get'/login' do
+  user = User.find_by(email: params[:email])
+  given_password = params[:password]
+  if user.password. == given_password
+    session[:user_id] = user.id
+    session[:user_name] = user.name
+    redirect '/profile'
+  else
+    flash[:error] = "Correct email, but wrong password. Did you mean: #{user.password}?\
+    Only use this password if that is your account."
+    redirect'/signup'
+ end
+end
+
+get '/signup' do
+  erb :signup
+end
+post '/signup' do
+  @user = User.new(params[:user])
+ if @user.valid?
+   @user.save
+   redirect:'/profile'
+ else
+  flash[:error] = @user.errors.full_messages;
+  redirect:'/signup'
+ end
+ p params
+end
+
+
+
+get '/profile/:id' do
+   @user = User.find(params[:id])
+erb :profile
+ end
+
+
+get'/logout' do
+  session[:user_id] = null
+  redirect'/'
+end
